fca0e3c03699826fe643775be62066fb82a72d6a
Thomas Fessel <tadeltaste@web.de>
2017-06-14 11:46:29 +0200

    moved configs to seperate file

---
+++ Config.lua
@@ -0,0 +1,10 @@
+
+-- temporary options
+
+
+tyrPlates.hideFriendlyHealthbar = true
+tyrPlates.hideFriendlyCastbar = true
+
+-- if the hp in percent of a friendly unit is below this, the healthbar is always shown
+tyrPlates.isLow = 50
+
