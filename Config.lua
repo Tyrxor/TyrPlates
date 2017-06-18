4f44e1e693126545baf80a0ed1a8e5f109314b9d
Thomas Fessel <tadeltaste@web.de>
2017-06-18 10:24:14 +0200

    changed minor stuff for testing

--- Config.lua
+++ Config.lua
@@ -1,9 +1,12 @@
 
 -- temporary options
 
-
+-- hide nameplate elements
 tyrPlates.hideFriendlyHealthbar = false
 tyrPlates.hideFriendlyCastbar = false
 
 -- if the hp in percent of a friendly unit is below this, the healthbar is always shown
 tyrPlates.isLow = 50
+
+-- 
+tyrPlates.filterFriendlyCasts = false
